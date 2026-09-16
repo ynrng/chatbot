5b80c3bdd8eb93a30713c7b90027f25998fe84ff
yan <yanrong.wang@ed.ac.uk>
2026-09-16 11:46:24 +0100

    update tile

--- components/travel/flightMap.tsx
+++ components/travel/flightMap.tsx
@@ -5,7 +5,7 @@ import L from "leaflet";
 
 import { MapContainer, TileLayer, useMap, useMapEvents } from "react-leaflet";
 
-// Fix Leaflet's default marker icon path
+// Fix Leaflet's default marker icon path,
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
@@ -36,14 +36,12 @@ export default function FlightMap({
             scrollWheelZoom={true}
             className="size-full flex flex-1 z-20"
         >
-            {/* <TileLayer
-                attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-            /> */}
 
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?token=6b0b8afe-3fa5-4569-a017-681170f87d5f"
+                minZoom={0}
+                maxZoom={20}
             />
             {children}
         </MapContainer>
